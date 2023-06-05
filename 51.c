341bf5771afecb66f53841a373fb83bbc41a448f
abhin_2122003 <abhinshelly@gmail.com>
2023-06-05 18:38:26 +0530

    done

--- 51.c
+++ 51.c
@@ -2,6 +2,7 @@
 //  S= X+X2/2 !+X4/4!+X6/6 !+……….
 
 #include<stdio.h>
+#include<math.h>
 
 int factorial(int n){
     int fact;
@@ -21,14 +22,14 @@ void main(){
     scanf("%f%d",&x,&n);
 
     float sum = x;
-    float num = x;
+    float num;
     float den = 1;
 
     int factorial(int);
 
     for(int i =1 ;i<n;i++){
 
-        num = num*x;
+        num = pow(x,(2*i));
         den = factorial(2*i);
         sum += num/den;
     }
@@ -37,3 +38,4 @@ void main(){
     printf("\n\nSum : %.2f\n\n",sum);
 
 }
+
